45f6014625b4c30ef89991f2c680b751b4b50f55
alwysaid <164992429+alwysaid@users.noreply.github.com>
2024-06-24 21:45:05 +0700

    latihan responsive

---
+++ index33.js
@@ -0,0 +1,21 @@
+/**
+ * responsive design
+ * 
+ * media query untuk handle responsive design
+ * 
+ *  @media only screen and (max-width: 600px) {
+  body {
+    background-color: lightblue;
+  }
+}
+
+unit 
+
+    absolute : px, cm, mm, in, pt, pc
+
+    relative : em , rem 
+    em = parent
+    rem = ngikutin em (child)
+
+ * 
+ */
